fe319c71643e67786e56764ba67050f884d9f350
MacBook <macbook@MacBook-Pro-MacBook.local>
2019-07-24 10:41:24 +0300

    not finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+import Colections from './pages/Colections';
+import Error from './pages/Error';
 
-const App = () => <p>Hello</p>;
+const App = () => (
+  <>
+    <ul>
+      <li>
+        <Link exact to="/">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/About">About</Link>
+      </li>
+      <li>
+        <Link to="/Colections">Colections</Link>
+      </li>
+    </ul>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/Colections" component={Colections} />
+      <Route component={Error} />
+    </Switch>
+  </>
+);
 
 export default App;
---
+++ src/components/pages/Colections.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Colections extends Component {
+  state = {};
+
+  render() {
+    return <p>Colections</p>;
+  }
+}
+
+export default Colections;
